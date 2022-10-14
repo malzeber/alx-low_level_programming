84ef2ab87ff4097fba019dc011973c7dd8def7dd
malzeber <m.alzeber97@gmail.com>
2022-10-13 18:28:50 -0700

    Rask101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main -Emtry
- * Retutn: 1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
 	return (1);
 }
