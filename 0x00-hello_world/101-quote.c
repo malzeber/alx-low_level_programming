d2a1ad01f46cd5c8cda19bc686bab7ee2e803a38
malzeber <m.alzeber97@gmail.com>
2022-10-13 18:39:39 -0700

    Rask101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,10 @@
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <unistd.h>
+
 /**
  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  *                followed by a new line, to standard error.
@@ -7,6 +13,6 @@
  */
 int main(void)
 {
-	(2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
