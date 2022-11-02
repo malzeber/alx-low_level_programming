65612c8910f41a63c28a0865b5eb4c218ff13c45
malzeber <m.alzeber97@gmail.com>
2022-11-02 12:49:21 -0700

    Task00

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,12 +4,12 @@
  * _put_recurdion - prints a string, followed by a new line.
  * @s: The string to be printed.
  */
-void_puts_recursion(char *s)
+void _puts_recursion(char *s);
 {
 	if (*s)
 	{
 		_putchar(*s);
-		_put_recursion(s + 1);
+		_puts_recursion(s + 1);
 	}
 
 	else
